4a57b7c00e54edd864be9079969522e4fbe44be3
nam <yhnham@gmail.com>
2025-07-30 22:49:06 +0900

    first commit

---
+++ src/main/java/com/example/youthCare/User/UserService.java
@@ -0,0 +1,4 @@
+package com.example.youthCare.User;
+
+public class UserService {
+}
---
+++ src/main/java/com/example/youthCare/YouthCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.youthCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthCareApplication.class, args);
+	}
+
+}
