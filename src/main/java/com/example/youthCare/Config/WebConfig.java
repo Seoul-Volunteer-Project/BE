d27553ba2e1ae36d4b092f89af9b6299f806ff98
nam <yhnham@gmail.com>
2025-08-03 09:11:52 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/example/youthCare/Config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.youthCare.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://seoul-volunteer-forked.vercel.app", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
